f580757714bc3ec7b8894beeead4ea4d3994b0c0
Moamen Hredeen <moamen@hredeen.com>
2025-04-14 22:39:57 +0200

    add dummy settings page

---
+++ apps/web/src/app/features/settings/settings.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatFormFieldModule, MatLabel } from '@angular/material/form-field';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+
+@Component({
+  selector: 'app-settings',
+  templateUrl: './settings.component.html',
+  styleUrl: './settings.component.scss',
+  imports: [
+    MatFormFieldModule,
+    MatCheckboxModule,
+    MatRadioModule,
+    MatSlideToggleModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatLabel
+  ],
+})
+export class SettingsComponent {
+
+  pageSize = new FormControl(10);
+  alwaysStartFromFirstPage = new FormControl(false);
+  darkMode = new FormControl(false);
+}
