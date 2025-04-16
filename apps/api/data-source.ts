7c91e97b7dfe1675560595d1b738fd553651d6a1
Moamen Hraden <moamen.hraden@primion.eu>
2025-04-16 12:07:47 +0200

    switch postgres

--- apps/api/data-source.ts
+++ apps/api/data-source.ts
@@ -3,14 +3,16 @@ import { IssueMigrations1710433714000 } from "./features/issue/issue.migrations"
 import { Issue } from "./features/issue/issue.models";
 import { IssueStatus } from "./features/issue/issue.models";
 import { SeedDataMigration1710433714100 } from "./seed-data.migration";
+import { appConfig } from "./config";
 
 export const AppDataSource = new DataSource({
-    type: "mysql",
-    host: "localhost",
-    port: 3306,
-    username: "root",
-    password: "root-password",
-    database: "issue_tracker",
+    type: "postgres",
+    ssl: true,
+    host: appConfig.database.host,
+    port: appConfig.database.port,
+    username: appConfig.database.user,
+    password: appConfig.database.password,
+    database: appConfig.database.name,
     entities: [Issue, IssueStatus],
     migrations: [
         IssueMigrations1710433714000, 
