7f144c35dce50a04c98861fc9f89031fc70b6118
Moamen Hraden <moamen.hraden@primion.eu>
2025-04-17 18:14:35 +0200

    update routes

--- apps/web/src/app/app.routes.ts
+++ apps/web/src/app/app.routes.ts
@@ -1,17 +1,32 @@
 import { Routes } from '@angular/router';
-
+import { LayoutComponent } from './layout/layout.component';
 export const routes: Routes = [
     {
-        path: 'issues',
-        loadComponent: () => import('./features/issue/issue.component').then(m => m.IssueComponent)
+        path: '',
+        component: LayoutComponent,
+        children: [
+            {
+                path: 'issues',
+                loadComponent: () => import('./features/issue/issue.component').then(m => m.IssueComponent)
+            },
+
+            {
+                path: 'settings',
+                loadComponent: () => import('./features/settings/settings.component').then(m => m.SettingsComponent)
+            },
+            {
+                path: 'identity',
+                loadChildren: () => import('./features/identity/identity.routes').then(m => m.identityRoutes)
+            },
+        ]
     },
     {
-        path: 'settings',
-        loadComponent: () => import('./features/settings/settings.component').then(m => m.SettingsComponent)
+        path: 'login',
+        loadComponent: () => import('./features/auth/login/login.component').then(m => m.LoginComponent)
     },
     {
-        path: 'identity',
-        loadChildren: () => import('./features/identity/identity.routes').then(m => m.identityRoutes)
+        path: 'register',
+        loadComponent: () => import('./features/auth/register/register.component').then(m => m.RegisterComponent)
     },
     {
         path: '',
