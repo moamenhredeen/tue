dc9bf1a33d28806a997802820a2a52a600c9f519
Moamen Hraden <moamen.hraden@primion.eu>
2025-04-17 18:14:16 +0200

    move layout to extra component

--- apps/web/src/app/app.component.ts
+++ apps/web/src/app/app.component.ts
@@ -1,19 +1,12 @@
 import { Component } from '@angular/core';
-import { MatButtonModule } from '@angular/material/button';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatListModule } from '@angular/material/list';
-import { RouterLink, RouterOutlet } from '@angular/router';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
   imports: [
-    RouterOutlet,
-    MatSidenavModule,
-    MatButtonModule,
-    MatListModule,
-    RouterLink
+    RouterOutlet
   ],
 })
 export class AppComponent {
