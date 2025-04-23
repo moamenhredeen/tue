b9d399aeb4fbd90ad6a3d853a8848bbf3d7c71b9
Moamen Hraden <moamen.hraden@primion.eu>
2025-04-23 02:13:58 +0200

    format and start using camel case instead of snake case

--- packages/api-spec/src/issue.types.ts
+++ packages/api-spec/src/issue.types.ts
@@ -1,42 +1,42 @@
-export type IssueStatusGroup = 'OPEN' | 'IN_PROGRESS' | 'CLOSED';
+export type IssueStatusGroup = 'OPEN' | 'IN_PROGRESS' | 'CLOSED'
 
 /**
  * Issue Status
  */
 export type IssueStatusResponse = {
-    id: number;
-    name: string;
-    description: string;
-    group: string;
+	id: number
+	name: string
+	description: string
+	group: string
 }
 
 /**
  * Create Issue Status Request
  */
 export type CreateIssueStatusRequest = {
-    name: string;
-    description: string;
-    group: string;
+	name: string
+	description: string
+	group: IssueStatusGroup
 }
 
 /**
  * Create Issue Request
  */
 export type CreateIssueRequest = {
-    title: string;
-    description: string;
-    status_id: number;
+	title: string
+	description: string
+	statusId: number
 }
 
 /**
  * Create Issue Response
  */
 export type IssueResponse = {
-    id: number;
-    title: string;
-    description: string;
-    status_id: number;
-    created_at: Date;
-    updated_at: Date;
-    status: IssueStatusResponse;
+	id: number
+	title: string
+	description: string
+	status_id: number
+	created_at: Date
+	updated_at: Date
+	status: IssueStatusResponse
 }
