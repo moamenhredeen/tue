7199627b2bc2176ae1ab3ef56ec3db39901fcd02
Moamen Hraden <moamen.hraden@primion.eu>
2025-05-06 11:27:31 +0200

    remove typescript files

--- apps/api/src/database/data-source.ts
+++
@@ -1,7 +0,0 @@
-import { appConfig } from '../../app.config'
-import { drizzle, NodePgDatabase } from 'drizzle-orm/node-postgres'
-
-export const appDataSource: NodePgDatabase = drizzle({
-	connection: appConfig.database.url,
-	casing: 'snake_case',
-})
--- apps/console/src/app/features/identity/identity.routes.ts
+++
@@ -1,9 +0,0 @@
-import { IdentityComponent } from "./identity.component";
-import { Routes } from "@angular/router";
-
-export const identityRoutes: Routes = [
-    {
-        path: '',
-        loadComponent: () => import('./identity.component').then(m => m.IdentityComponent)
-    }
-];
