1e4702eb2e3e1b04b9c0017f1eb12e547d091769
Moamen Hraden <moamen.hraden@primion.eu>
2025-11-22 10:25:03 +0100

    add login page

--- apps/console/src/main.ts
+++ apps/console/src/main.ts
@@ -2,10 +2,10 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import './style.css'
 
+import router from './router.js'
 import App from './App.vue'
-import router from './router'
 
-const app = createApp(App)
+const app = createApp(App).use(router)
 
 app.use(createPinia())
 app.use(router)
